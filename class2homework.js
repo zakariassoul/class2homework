db40af726a2f4ce0fe349bd5f887257554d054f9
Zak <zakassoul@zaks-mbp.ten.thomsonreuters.com>
2016-03-16 18:14:00 -0700

    added my homeowrk

--- class2homework.js
+++ class2homework.js
@@ -1,3 +1,12 @@
-function sayHello() {
-	return "hello" + "zak"
+var str = "wow";
+
+function checkPalindrome(str) {
+  if (str == str.split('').reverse().join('')) {
+  	return true
+  }
+  else {
+  	return false
+  }
 }
+
+console.log(checkPalindrome(str));
